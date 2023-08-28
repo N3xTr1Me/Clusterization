47f49ea9e0026dd2e3d6c7d56211f129945265c7
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2023-08-28 14:04:52 +0300

    Unit testing

--- testing/unit_testing/main.cpp
+++ testing/unit_testing/main.cpp
@@ -1,8 +1,12 @@
 #include "./data_structures/point_tests.h"
+#include "./data_structures/domain_tests.h"
+#include "./data_structures/target_tests.h"
 
 
 int main() {
 
-    point_testing();
+    Point_tests::run_tests();
+    Domain_tests::run_tests();
+    Target_tests::run_test();
 
 }
