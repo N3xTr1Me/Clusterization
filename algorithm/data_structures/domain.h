75172c5927d5fdbc57cfea119b959724f310548e
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2023-08-28 14:04:29 +0300

    Domain revamp + unit tests

--- algorithm/data_structures/domain.h
+++ algorithm/data_structures/domain.h
@@ -1,56 +1,121 @@
 #ifndef CLUSTERIZATION_DOMAIN_H
 #define CLUSTERIZATION_DOMAIN_H
 
-#include "point.h"
-#include <iostream>
+#include <stdexcept>
 
+#include "../configuration/config.h"
+#include "./point.h"
+
+// TODO: Заменить проброс ошибки на сохранение стейта
 
 // Непосредственно двумерная плоскость с точками на ней.
 class Domain {
 
+protected:
+
+    // Размеры области
     unsigned int x_size = 0;
     unsigned int y_size = 0;
 
-    Point data[Y_LIMIT][X_LIMIT];
+    // Массив точек
+    Point plane[Y_LIMIT][X_LIMIT];
 
 public:
 
     Domain() = default;
 
-    // Создание плоскости из массива константных размеров
-    explicit Domain(int data[Y_LIMIT][X_LIMIT]) : x_size(X_LIMIT), y_size(Y_LIMIT), data() {
+    // Конструктор области фиксированного размера
+    explicit Domain(int points[Y_LIMIT][X_LIMIT]) : x_size(X_LIMIT), y_size(Y_LIMIT), plane()
+    {
 
         for (int y = 0; y < this->y_size; ++y) {
             for (int x = 0; x < this->x_size; ++x) {
-                this->data[y][x] = Point(x, y, data[y][x]);
+                this->plane[y][x] = Point(x, y, points[y][x] > 0);
             }
         }
 
     }
 
-    // Создание плоскости из массива произвольных размеров
-    Domain(int** data, unsigned int x_size, unsigned int y_size) : x_size(x_size), y_size(y_size) {
+    // Создание области "произвольного" размера
+    Domain(int** points, unsigned int x_size, unsigned int y_size) : plane()
+    {
 
-        for (int y = 0; y < this->y_size; ++y) {
-            for (int x = 0; x < this->x_size; ++x) {
-                this->data[y][x] = Point(x, y, data[y][x]);
+        if (x_size > X_LIMIT) {
+            throw std::runtime_error("X-axis size exceeds limit of " + std::to_string(X_LIMIT));
+        }
+
+        if (y_size > Y_LIMIT) {
+            throw std::runtime_error("Y-axis size exceeds limit of " + std::to_string(Y_LIMIT));
+        }
+
+        this->x_size = x_size;
+        this->y_size = y_size;
+
+        for (int y = 0; y < y_size; ++y) {
+            for (int x = 0; x < x_size; ++x) {
+                this->plane[y][x] = Point(x, y, points[y][x] > 0);
             }
         }
 
-    };
+    }
 
-    // get-методы для размеров плоскости
+    // Конструктор копирования
+    Domain(Domain& other) {
+
+        this->x_size = other.x_size;
+        this->y_size = other.y_size;
+
+        for (int y = 0; y < this->y_size; ++y) {
+            for (int x = 0; x < this->x_size; ++x) {
+                this->plane[y][x] = other[y][x];
+            }
+        }
+
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Методы "геттеры"
+
     unsigned int get_x_size() const { return this->x_size; };
     unsigned int get_y_size() const { return this->y_size; };
 
-    void reassign(unsigned int x, unsigned y, ITarget* id) {
-            this->data[y][x].target_id = id;
-    };
+    // Оператор обращения к массиву (точек)
+    Point* operator[](unsigned int index) {
 
-    Point* operator[](unsigned int index) { return this->data[index]; };
+        if (index >= this->y_size) {
+            throw std::runtime_error("Index is out of bounds: " + std::to_string(index) +
+            " out of " + std::to_string(this->y_size)
+            );
+        }
 
-    ~Domain() = default;
+        return this->plane[index];
+
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    Domain& operator=(Domain other) {
+
+        if (this != &other) {
+
+            this->x_size = other.x_size;
+            this->y_size = other.y_size;
+
+            for (int y = 0; y < this->y_size; ++y) {
+                for (int x = 0; x < this->x_size; ++x) {
+                    this->plane[y][x] = other[y][x];
+                }
+            }
+
+        }
+
+        return *this;
+    }
+
+    virtual ~Domain() = default;
 
 };
+
 
 #endif //CLUSTERIZATION_DOMAIN_H
---
+++ testing/unit_testing/data_structures/domain_tests.h
@@ -0,0 +1,169 @@
+#ifndef CLUSTERIZATION_DOMAIN_TESTS_H
+#define CLUSTERIZATION_DOMAIN_TESTS_H
+
+#define TEST_CASES 7
+#define X_LIMIT 20
+#define Y_LIMIT 10
+
+#include <cassert>
+#include <iostream>
+#include <random>
+
+#include "../../../algorithm/data_structures/domain.h"
+
+
+namespace Domain_tests {
+
+    class TestDomain : public Domain {
+
+    public:
+
+        // Костыль Для тестирования
+        Point* hard_get(unsigned int x, unsigned int y) noexcept {
+
+            if (x >= X_LIMIT) {
+                return nullptr;
+            }
+
+            if (y >= Y_LIMIT) {
+                return nullptr;
+            }
+
+            return &this->plane[y][x];
+        };
+
+        ~TestDomain() override = default;
+    };
+
+    void default_construction() {
+
+        TestDomain sample{};
+
+        assert(sample.get_x_size() == 0);
+        assert(sample.get_y_size() == 0);
+
+        std::cout << "Default construction size assertion: OK" << std::endl;
+
+        for (unsigned int y = 0; y < Y_LIMIT; ++y) {
+            for(unsigned int x = 0; x < X_LIMIT; ++x) {
+                Point* dot = sample.hard_get(x, y);
+
+                if (dot == nullptr) {
+                    throw std::runtime_error("Received a null point (nullptr)! "
+                                             "This should not happen, as Point struct provides a default constructor.");
+                }
+
+                assert(dot->target_id == nullptr);
+                assert(dot->valid == false);
+                assert(dot->x == 0);
+                assert(dot->y == 0);
+            }
+        }
+
+        std::cout << "Default construction point data assertion: OK" << std::endl;
+
+    }
+
+    void fixed_size_construction() {
+
+        int points[Y_LIMIT][X_LIMIT] = {0};
+
+        int cases[TEST_CASES][2] = {
+                {11, 2},
+                {0, 0},
+                {17, 9},
+                {9, 9},
+                {19, 8},
+                {19, 0},
+                {0, 9}
+        };
+
+        for (auto test : cases) {
+            points[test[1]][test[0]] = 1;
+        }
+
+        Domain sample{points};
+
+        for (auto test : cases) {
+            Point original = sample[test[1]][test[0]];
+
+            // На всякий случай...
+            assert(original.x == test[0]);
+            assert(original.y == test[1]);
+
+            assert(original.target_id == nullptr);
+            assert(original.valid == true);
+        }
+
+        std::cout << "Fixed size construction: OK" << std::endl;
+
+    }
+
+    void _generate_domain(int** data, unsigned int x_size, unsigned int y_size) {
+
+        if (x_size > X_LIMIT) {
+            std::cout << "[Domain generation] X coordinate out of bounds! (replaced with default value)" << std::endl;
+            x_size = X_LIMIT;
+        }
+
+        if (y_size > Y_LIMIT) {
+            std::cout << "[Domain generation] Y coordinate out of bounds! (replaced with default value)" << std::endl;
+            y_size = Y_LIMIT;
+        }
+
+        std::random_device rd;
+        std::mt19937 rng(rd());
+        std::uniform_int_distribution<std::mt19937::result_type> distribution(0, 1);
+
+        for (unsigned int y = 0; y < y_size; ++y) {
+            for (unsigned int x = 0; x < x_size; ++x) {
+                data[y][x] = distribution(rng);
+            }
+        }
+
+    }
+
+    void custom_size_construction() {
+
+        int test_case[Y_LIMIT][X_LIMIT];
+
+        int* test[Y_LIMIT];
+
+        for (unsigned int i = 0; i < Y_LIMIT; ++i) {
+            test[i] = test_case[i];
+        }
+
+        _generate_domain(test, X_LIMIT, Y_LIMIT);
+
+        Domain sample{test, X_LIMIT, Y_LIMIT};
+
+        for (unsigned int y = 0; y < Y_LIMIT; ++y) {
+            for (unsigned int x = 0; x < X_LIMIT; ++x) {
+
+                if (test_case[y][x] > 0) {
+                    assert(sample[y][x].valid == true);
+                }
+
+            }
+        }
+
+        std::cout << "Custom size construction: OK" << std::endl;
+
+    }
+
+    void run_tests() {
+
+        std::cout << "\nDomain data structure testing: \n{" << std::endl;
+
+        default_construction();
+        fixed_size_construction();
+        custom_size_construction();
+
+        std::cout << "}\n" << std::endl;
+
+    }
+
+}
+
+
+#endif //CLUSTERIZATION_DOMAIN_TESTS_H
