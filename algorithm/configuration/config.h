def0fdee8d0dbb8df6ae3cdc385d87e7c57e4c45
N3xTr1Me <destriker17@gmail.com>
2023-10-17 15:50:15 +0300

    Config rudimentary class cleanup

--- algorithm/configuration/config.h
+++ algorithm/configuration/config.h
@@ -1,6 +1,7 @@
 #ifndef CLUSTERIZATION_CONFIG_H
 #define CLUSTERIZATION_CONFIG_H
 
+
 #define X_LIMIT 4000        // Максимальное значение по оси X
 #define Y_LIMIT 36          // Максимальное значение по оси Y
 
@@ -9,34 +10,6 @@
 
 #define TARGET_LIMIT 10     // Максимальное число целей, на которое алгоритм разобьет точки на плоскости
 #define POINT_LIMIT 15      // Максимум точек в отдельной цели
-
-
-struct AlgorithmConfig {
-
-    const int x_epsilon = X_SHIFT;
-    const int y_epsilon = Y_SHIFT;
-
-    const unsigned int targets_max = TARGET_LIMIT;
-    const unsigned int points_max = POINT_LIMIT;
-
-    AlgorithmConfig() = default;
-
-    //------------------------------------------------------------------------------------------------------------------
-    // Конструкторы кастомизируемых конфигураций
-    AlgorithmConfig(int x_shift, int y_shift) :
-    x_epsilon(x_shift), y_epsilon(y_shift), targets_max(TARGET_LIMIT), points_max(POINT_LIMIT) {};
-
-    AlgorithmConfig(unsigned int max_targets, unsigned int max_points) :
-    x_epsilon(X_SHIFT), y_epsilon(Y_SHIFT), targets_max(max_targets), points_max(max_points) {};
-
-    AlgorithmConfig(int x_shift, int y_shift, unsigned int max_targets, unsigned int max_points) :
-    x_epsilon(x_shift), y_epsilon(y_shift), targets_max(max_targets), points_max(max_points) {};
-
-    //------------------------------------------------------------------------------------------------------------------
-
-    ~AlgorithmConfig() = default;
-
-};
 
 
 #endif //CLUSTERIZATION_CONFIG_H
